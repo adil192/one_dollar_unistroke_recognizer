b73d2fb1cb0e8372e300404770f8e3441df10f2d
Adil Hanney <adilh+github@orchid.anonaddy.com>
2023-11-27 21:21:37 +0000

    test: test all shapes in reverse

--- test/known_unistrokes_test.dart
+++ test/known_unistrokes_test.dart
@@ -2,28 +2,24 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:one_dollar_unistroke_recognizer/one_dollar_unistroke_recognizer.dart';
 
 void main() {
-  group('known unistrokes should be recognized (golden section search)', () {
-    for (var unistroke in default$1Unistrokes) {
-      test(unistroke.name, () {
-        final result = recognizeUnistroke(
-          unistroke.points,
-          useProtractor: false,
-        );
-        expect(result, isNotNull);
-        expect(result!.name, unistroke.name);
-      });
-    }
-  });
-  group('known unistrokes should be recognized (protractor)', () {
-    for (var unistroke in default$1Unistrokes) {
-      test(unistroke.name, () {
-        final result = recognizeUnistroke(
-          unistroke.points,
-          useProtractor: true,
-        );
-        expect(result, isNotNull);
-        expect(result!.name, unistroke.name);
-      });
+  group('known unistrokes should be recognized', () {
+    for (final useProtractor in [false, true]) {
+      for (final reversed in [false, true]) {
+        for (final unistroke in default$1Unistrokes) {
+          test(
+            '${unistroke.name} (useProtractor: $useProtractor, '
+            'reversed: $reversed)',
+            () {
+              final result = recognizeUnistroke(
+                reversed ? unistroke.reversedPoints : unistroke.points,
+                useProtractor: useProtractor,
+              );
+              expect(result, isNotNull);
+              expect(result!.name, unistroke.name);
+            },
+          );
+        }
+      }
     }
   });
 }
